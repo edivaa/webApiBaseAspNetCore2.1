f99ad86c5adf63c42c675fec36c41e4fda10d6b3
Edivã <ediva.teodozio@savaldor.ba.gov.br>
2019-03-22 15:50:58 -0300

    Configuracao de conexao

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-
+using Microsoft.EntityFrameworkCore;
+using webApiBaseAspNetCore2._1.Data;
 namespace webApiBaseAspNetCore2._1
 {
     public class Startup
@@ -25,6 +26,7 @@ namespace webApiBaseAspNetCore2._1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
