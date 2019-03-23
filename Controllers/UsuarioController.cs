84a5af87db739354eb64f449046682414441de36
Edivã Gonçalves <edivaa@gmail.com>
2019-03-23 08:56:12 -0300

    Pegando usuario do base de dados

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-using webApiBaseAspNetCore2._1.Services;
 using webApiBaseAspNetCore2._1.Models;
+using webApiBaseAspNetCore2._1.Data;
 
 namespace webApiBaseAspNetCore2._1.Controllers
 {
@@ -14,25 +14,47 @@ namespace webApiBaseAspNetCore2._1.Controllers
     public class UsuarioController : ControllerBase
     {
        
-         
+        private readonly TodoContext _context; 
+
+        public UsuarioController(TodoContext context){
+            _context = context;
+
+            if(_context.Usuarios.Count()==0){
+
+                _context.Usuarios.Add(
+                    new Usuario { 
+                        Nome="Antonio",
+                        Telefone="321212313",
+                        Email="anto@gmail.com"
+                    }
+                );
+
+                _context.SaveChanges();
+            }
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<Usuario>> Get()
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
         {
-            return  UsuarioService.GetDadosUsuarios();
+            return  await _context.Usuarios.ToListAsync();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<string> GetUsuario(int id)
         {
             return "value";
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Usuario>> Post(Usuario value)
         {
+            _context.Usuario.Add(value);
+            await _context.SaveChanges();
+
+            return CreatedAtAction(nameof(Get), new {Id=value.Id},value);
         }
 
         // PUT api/values/5
