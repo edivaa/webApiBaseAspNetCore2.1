7b304b44d2a254766f116ec8cc3ab4dd095d663a
Edivã Gonçalves <edivaa@gmail.com>
2019-03-23 08:56:36 -0300

    Ajuste para retorno de usuario

--- Services/UsuarioService.cs
+++ Services/UsuarioService.cs
@@ -1,7 +1,7 @@
 
 using System.Collections.Generic;
 using webApiBaseAspNetCore2._1.Models;
-
+using webApiBaseAspNetCore2._1.Data;
 
 
 
@@ -10,55 +10,56 @@ namespace webApiBaseAspNetCore2._1.Services
 
 public class UsuarioService {
 
- public static List<Usuario> GetDadosUsuarios()
-        {
-            List<Usuario> dados = new List<Usuario>();
 
-            dados.Add(new Usuario()
-            {
-                Id=1, 
-                Nome="Antonio",
-                Telefone="321212313",
-                Email="anto@gmail.com"
-            });
+        public static List<Usuario> GetDadosUsuarios()
+                {
+                    List<Usuario> dados = new List<Usuario>();
+
+                    dados.Add(new Usuario()
+                    {
+                        Id=1, 
+                        Nome="Antonio",
+                        Telefone="321212313",
+                        Email="anto@gmail.com"
+                    });
 
 
-           dados.Add(new Usuario()
-            { 
-                Id=2,
-                Nome="Paulo",
-                Telefone="321212313",
-                Email="paulo@gmail.com"
-            });
+                dados.Add(new Usuario()
+                    { 
+                        Id=2,
+                        Nome="Paulo",
+                        Telefone="321212313",
+                        Email="paulo@gmail.com"
+                    });
 
-             dados.Add(new Usuario()
-            { 
-                Id=3,
-                Nome="Fernando",
-                Telefone="99987889",
-                Email="fernC@gmail.com"
-            });
+                    dados.Add(new Usuario()
+                    { 
+                        Id=3,
+                        Nome="Fernando",
+                        Telefone="99987889",
+                        Email="fernC@gmail.com"
+                    });
 
-             dados.Add(new Usuario()
-            { 
-                Id=4,
-                Nome="Marcelo",
-                Telefone="998989898",
-                Email="marcelo@gmail.com"
-            });
+                    dados.Add(new Usuario()
+                    { 
+                        Id=4,
+                        Nome="Marcelo",
+                        Telefone="998989898",
+                        Email="marcelo@gmail.com"
+                    });
 
-              dados.Add(new Usuario()
-            { 
-                Id=5,
-                Nome="Antenou",
-                Telefone="335639993",
-                Email="antenou@gmail.com"
-            });
+                    dados.Add(new Usuario()
+                    { 
+                        Id=5,
+                        Nome="Antenou",
+                        Telefone="335639993",
+                        Email="antenou@gmail.com"
+                    });
 
-            return dados;
+                    return dados;
 
-        }
-    }
+                }
+            }
 
 }
 
